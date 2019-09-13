a1690e301230d554c97d819c8bd36f0cf53a4966
Steget <sthugo@student.chalmers.se>
2019-09-13 16:16:33 +0200

    Start of implementing JavaFX
    Not in a runnable state atm.

--- src/main/java/edu/chalmers/projecttemplate/ProjectTemplate.java
+++ src/main/java/edu/chalmers/projecttemplate/ProjectTemplate.java
@@ -3,15 +3,31 @@ package edu.chalmers.projecttemplate;
 import edu.chalmers.projecttemplate.controller.ProjectController;
 import edu.chalmers.projecttemplate.model.Project;
 import edu.chalmers.projecttemplate.view.ProjectView;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import javax.swing.SwingUtilities;
 
-public final class ProjectTemplate {
+public final class ProjectTemplate extends Application {
 	private ProjectTemplate() {
 		/* No instances allowed! */
 	}
 
+	@Override
+	public void start(Stage stage) throws Exception {
+			Parent root = FXMLLoader.load(getClass().getResource("storageSystem.fxml"));
+
+			Scene scene = new Scene(root);
+
+			stage.setScene(scene);
+			stage.show();
+	}//memo
+
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
+		/*SwingUtilities.invokeLater(new Runnable() {
 
 			@Override
 			public void run() {
@@ -21,6 +37,8 @@ public final class ProjectTemplate {
 				ProjectController.create(project, projectView);
 				projectView.setVisible(true);
 			}
-		});
+		});*/
+
+		launch(args);
 	}
 }
--- src/main/java/edu/chalmers/projecttemplate/controller/ProjectController.java
+++ src/main/java/edu/chalmers/projecttemplate/controller/ProjectController.java
@@ -2,11 +2,14 @@ package edu.chalmers.projecttemplate.controller;
 
 import edu.chalmers.projecttemplate.model.Project;
 import edu.chalmers.projecttemplate.view.ProjectView;
+import javafx.fxml.Initializable;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ProjectController {
+public class ProjectController implements Initializable {
   private final Project project;
   private final ProjectView projectView;
   public static final int KO = 1;
@@ -28,5 +31,10 @@ public class ProjectController {
       project.incrementPresses();
       projectView.getPressesLabel().setText(String.valueOf(project.getPresses()));
     }
+  }
+
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+
   }
 }
