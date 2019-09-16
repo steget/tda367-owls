de14b95bae524b8438b5a6af75dcde5272b3271a
Steget <sthugo@student.chalmers.se>
2019-09-16 10:48:39 +0200

    Remove all swing files

--- src/main/java/edu/chalmers/projecttemplate/ProjectTemplate.java
+++ src/main/java/edu/chalmers/projecttemplate/ProjectTemplate.java
@@ -1,44 +1,24 @@
 package edu.chalmers.projecttemplate;
 
-import edu.chalmers.projecttemplate.controller.ProjectController;
-import edu.chalmers.projecttemplate.model.Project;
-import edu.chalmers.projecttemplate.view.ProjectView;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import javax.swing.SwingUtilities;
+public class ProjectTemplate extends Application {
 
-public final class ProjectTemplate extends Application {
-	/*private ProjectTemplate() {
-		*//* No instances allowed! *//*
-	}*/
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/resources/storageSystem.fxml"));
 
-	@Override
-	public void start(Stage stage) throws Exception {
-			Parent root = FXMLLoader.load(getClass().getResource("/resources/storageSystem.fxml"));
+        Scene scene = new Scene(root);
 
-			Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
-			stage.setScene(scene);
-			stage.show();
-	}//memo
-
-	public static void main(String[] args) {
-		/*SwingUtilities.invokeLater(new Runnable() {
-
-			@Override
-			public void run() {
-				final Project project = new Project();
-				final ProjectView projectView = new ProjectView(project);
-
-				ProjectController.create(project, projectView);
-				projectView.setVisible(true);
-			}
-		});*/
-
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/edu/chalmers/projecttemplate/controller/ProjectController.java
+++ src/main/java/edu/chalmers/projecttemplate/controller/ProjectController.java
@@ -10,28 +10,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ProjectController implements Initializable {
-  //private final Project project;
-  //private final ProjectView projectView;
-  public static final int KO = 1;
+    private Project project;
+    private ProjectView projectView;
 
-  /*public static ProjectController create(Project project, ProjectView projectView) {
-    return new ProjectController(project, projectView);
-  }*/
-
-
-
-  private class ProjectButtonPressed implements ActionListener {
     @Override
-    public void actionPerformed(ActionEvent e) {
-  //    project.incrementPresses();
-  //    projectView.getPressesLabel().setText(String.valueOf(project.getPresses()));
+    public void initialize(URL url, ResourceBundle rb) {
+
     }
-  }
-
-  @Override
-  public void initialize(URL url, ResourceBundle rb) {
-
-
-
-  }
 }
