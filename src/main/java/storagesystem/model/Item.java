5d004e7e32b1515a4618788a3aa3eb9521f0c54a
Kvick <kvickstyler@gmail.com>
2019-09-23 17:04:16 +0200

    Added neccessary properties to Item class

--- src/main/java/storagesystem/model/Item.java
+++ src/main/java/storagesystem/model/Item.java
@@ -1,4 +1,87 @@
 package storagesystem.model;
 
+import javafx.scene.image.Image;
+
 public class Item {
+    private int ID;
+    private String name;
+    private Location location;
+    private int amount;
+    private String description;
+    private Image image;
+    private int condition;
+    private String userRequirements;
+    private boolean reservable;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public String getUserRequirements() {
+        return userRequirements;
+    }
+
+    public void setUserRequirements(String userRequirements) {
+        this.userRequirements = userRequirements;
+    }
+
+    public boolean isReservable() {
+        return reservable;
+    }
+
+    public void setReservable(boolean reservable) {
+        this.reservable = reservable;
+    }
 }
