907a0b755fcc65978969862561cdc66ccd6a3407
Kvick <kvickstyler@gmail.com>
2019-09-26 14:31:53 +0200

    Added Functionality to set image
    also added some imports for javaFX

--- src/main/java/storagesystem/controller/ProjectController.java
+++ src/main/java/storagesystem/controller/ProjectController.java
@@ -1,13 +1,21 @@
 package storagesystem.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ProjectController implements Initializable {
+    @FXML
+    private ImageView imageView;
+    @FXML
+    private Label itemPageName;
 
     public void initialize(URL url, ResourceBundle rb) {
-
+        imageView.setImage(new Image("art.png"));
     }
 }
