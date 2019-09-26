4663441d55c885e45d1d29452318f7a4d3ce2ff9
Jonathan Eksberg <jonathan.eksberg@gmail.com>
2019-09-26 15:07:54 +0200

    Added the current existing fxml-ID's for itemPage.fxml

--- src/main/java/storagesystem/controller/ItemPageController.java
+++ src/main/java/storagesystem/controller/ItemPageController.java
@@ -3,6 +3,8 @@ package storagesystem.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -12,12 +14,28 @@ import java.util.ResourceBundle;
 public class ItemPageController implements Initializable {
 
     @FXML
-    private ImageView imageView;
+    private ImageView itemPageImageView;
     @FXML
-    private Label itemPageName;
+    private Label itemPageNameLabel;
+    @FXML
+    private Label itemPageIDLabel;
+    @FXML
+    private Label itemPageAmountLabel;
+    @FXML
+    private Label itemPageLocationLabel;
+    @FXML
+    private Label itemPageTeamOwnerLabel;
+    @FXML
+    private Label itemPageReservableLabel;
+    @FXML
+    private Slider itemPageConditionSlider;
+    @FXML
+    private TextArea itemPageDescriptionTA;
+    @FXML
+    private TextArea itemPageUserRequirementsTA;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        imageView.setImage(new Image("art.png"));
+        itemPageImageView.setImage(new Image("art.png"));
     }
 }
