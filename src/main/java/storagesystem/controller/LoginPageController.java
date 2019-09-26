fc2f5d0e8ea8c4a1fe77ca9364c580af1b56d03f
Steget <sthugo@student.chalmers.se>
2019-09-26 15:46:29 +0200

    Start of GUI for a login page

---
+++ src/main/java/storagesystem/controller/LoginPageController.java
@@ -0,0 +1,32 @@
+package storagesystem.controller;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginPageController implements Initializable {
+
+    @FXML
+    TextField userNameTextField;
+
+    @FXML
+    TextField passwordTextField;
+
+    @FXML
+    ChoiceBox organisationChoiceBox;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //todo Fill list with organisations from database
+        ObservableList<String> organisations = FXCollections.observableArrayList("Informationsteknik", "Data");
+
+        organisationChoiceBox.setItems(organisations);
+        organisationChoiceBox.setValue(organisations.get(0)); //show first value in box
+    }
+}
