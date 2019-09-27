45ba7433f04b9c78028c31042efd994756fa538c
Pär Aronsson <per_a99@hotmail.com>
2019-09-27 11:56:59 +0200

    created the GUI for the settings

--- src/main/java/storagesystem/controller/SettingsController.java
+++ src/main/java/storagesystem/controller/SettingsController.java
@@ -1,27 +1,67 @@
 package storagesystem.controller;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import sun.plugin.javascript.navig.Anchor;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.EventListener;
 import java.util.ResourceBundle;
 
 public class SettingsController implements Initializable {
 
     @FXML
-    private Label settingLabel;
+    Label settingsLabel;
 
+    @FXML
+    AnchorPane settingsTeamAnchorPane;
+
+    @FXML
+    AnchorPane settingsUserAnchorPane;
+
+    @FXML
+    Label settingsUserLabel;
+
+    @FXML
+    Label settingsTeamLabel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        settingLabel.setText("Hello World!");
+        settingsLabel.setText("Hello World!");
+
+
+        settingsUserLabel.setOnMouseClicked((event -> {
+
+            if(!settingsUserAnchorPane.isVisible()) {
+                settingsUserAnchorPane.setVisible(true);
+                settingsTeamAnchorPane.setVisible(false);
+            }
+        }));
+        settingsTeamLabel.setOnMouseClicked((event -> {
+            if(settingsUserAnchorPane.isVisible()) {
+                settingsUserAnchorPane.setVisible(false);
+                settingsTeamAnchorPane.setVisible(true);
+            }
+
+        }));
+
 
 
 
 
     }
-}
+
+
+
+
+
+
+    }
+
+
+
