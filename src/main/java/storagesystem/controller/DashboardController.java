31ea2a80182860e2b76188eac78724f55a69d4fd
Steget <sthugo@student.chalmers.se>
2019-09-27 13:14:11 +0200

    Connect dashboardPanel to settingsPanel

--- src/main/java/storagesystem/controller/DashboardController.java
+++ src/main/java/storagesystem/controller/DashboardController.java
@@ -1,13 +1,22 @@
 package storagesystem.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DashboardController implements Initializable {
+
+    @FXML
+    AnchorPane rootPane;
 
     @FXML
     Button settingsButton;
@@ -15,5 +24,13 @@ public class DashboardController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    @FXML
+    void settingsButtonPressed() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/settings.fxml"));
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
