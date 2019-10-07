a408d3296eeabae0ad953e7601aed7bf5dde1142
Kvick <kvickstyler@gmail.com>
2019-10-07 17:04:18 +0200

    Writing and reading from json file now possible

--- src/main/java/storagesystem/services/GSONWritingToFile.java
+++ src/main/java/storagesystem/services/GSONWritingToFile.java
@@ -1,24 +1,34 @@
 package storagesystem.services;
 
 import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
+import com.google.gson.GsonBuilder;
 import storagesystem.model.Item;
 
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GSONWritingToFile {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Gson gson = new Gson();
-        
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Item testItem = new Item();
+        Writer writer = new FileWriter("src/main/resources/package.json");
+        List<Item> itemList = new ArrayList<>();
 
+        testItem.setName("This is now in the json file");
 
-        /*String json = gson.toJson(testItem);
-        JsonReader reader = new JsonReader(new StringReader(json));
-        reader.setLenient(true);
-        Item testItem1 = gson.fromJson(json, Item.class);
-        System.out.print(testItem1.getName());
-        */
+        Item testItem2 = new Item();
+        itemList.add(testItem2);
+        itemList.add(testItem);
+        gson.toJson(itemList, writer);
+        writer.flush();
+        writer.close();
+
+        List itemList2 = gson.fromJson(new FileReader("src/main/resources/package.json"), ArrayList.class);
+
+        System.out.println(itemList2.get(1).getClass()); //TODO: Fix LinkedTreeMap (to Item)
     }
 }
