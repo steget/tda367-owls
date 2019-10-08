5df8eea1871f65628f77d63a32e712aba4ff1e85
William <wilalbertsson@gmail.com>
2019-10-08 12:25:24 +0200

    JavaDoc

--- src/main/java/storagesystem/model/IReservable.java
+++ src/main/java/storagesystem/model/IReservable.java
@@ -5,8 +5,19 @@ package storagesystem.model;
  */
 
 public interface IReservable {
+    /**
+     *
+     * @return An unique ID.
+     */
     int getID();
+
+    /**
+     * Should be implemented by all implementors
+     * @param o An object which to test against
+     * @return True if o is instance of and has the same ID's as this IReservable.
+     */
     @Override
     boolean equals(Object o);
+
     boolean isReservable();
 }
--- src/main/java/storagesystem/model/IReservation.java
+++ src/main/java/storagesystem/model/IReservation.java
@@ -7,18 +7,36 @@ import org.joda.time.Interval;
  */
 
 public interface IReservation {
+    /**
+     *
+     * @return The unique ID for this reservation
+     */
     int getID();
 
+    /**
+     *
+     * @return The party that borrows the object in Resercation
+     */
     IBorrower getBorrower();
 
+    /**
+     *
+     * @return The time interval when the reservation is
+     */
     Interval getInterval();
 
+
     IReservable getReservedObject();
+
 
     ReservationStatus getStatus();
 
 
-
+    /**
+     * Should be implemented in all implementors and should compare only the ID's
+     * @param o An object against which to test
+     * @return True if o is of the same class and has the same ID, otherwise false
+     */
     @Override
     boolean equals(Object o);
 }
