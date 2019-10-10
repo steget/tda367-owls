6dacf9b38c63936fc83ec0adee358c8fc9478ded
Kvick <kvickstyler@gmail.com>
2019-10-10 15:53:27 +0200

    fixed ItemPageController

--- src/main/java/storagesystem/controller/ItemPageController.java
+++ src/main/java/storagesystem/controller/ItemPageController.java
@@ -51,10 +51,10 @@ public class ItemPageController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resources) {
+        Location location = new Location("MockLocation", "This location does not exist", new Image("pictures/creepy.jpg"));
         Item mockItem = new Item("mockItem", "This is a description", "Behave please.", 1, 2, Condition.GOOD, true, location, location.getImage());
         Team mockTeam = new Team("mockTeam");
         setAll(mockItem, mockTeam);
-        Location location = new Location("MockLocation", "This location does not exist", new Image("pictures/creepy.jpg"));
     }
 
     /**
