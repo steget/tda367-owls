cc8a996a298d9f959ccb0b2a72b6d7a74647b8a9
Kvick <kvickstyler@gmail.com>
2019-10-10 15:54:36 +0200

    solved merge issues in GSONHandlerTest
    Also made actual tests.

--- src/test/java/storagesystem/services/GSONHandlerTest.java
+++ src/test/java/storagesystem/services/GSONHandlerTest.java
@@ -1,8 +1,11 @@
 package storagesystem.services;
 
+import org.junit.Assert;
+import org.junit.Test;
 import storagesystem.model.Condition;
 import storagesystem.model.Item;
-import storagesystem.services.GSONHandler;
+import storagesystem.model.Location;
+
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,17 +15,23 @@ import static storagesystem.services.GSONHandler.getListFromJson;
 
 public class GSONHandlerTest {
 
-    public static void main(String[] args) throws IOException { //TODO: make actual tests
+    @Test
+    public void shouldAddItemsToJSON() throws IOException {
         GSONHandler.clearJson("src/main/resources/json/itemDB.json");
-        List itemList = new ArrayList();
+        Location location = new Location();
+        List<Item> itemList = new ArrayList();
+
         Item item1 = new Item();
-        Item item2 = new Item("name", "description", "UserReq", 2, 10, Condition.GREAT, false);
+        Item item2 = new Item("name", "description", "UserReq", 2, 10, Condition.GREAT, false, location, location.getImage());
+
         itemList.add(item1);
         itemList.add(item2);
+
         GSONHandler.addListToJson(itemList, "src/main/resources/json/itemDB.json");
+
         List<Item> listFromJson = getListFromJson("src/main/resources/json/itemDB.json", Item.class);
-        System.out.println(listFromJson.get(0).getDescription());
+
+        Assert.assertTrue(listFromJson.get(0).getName().equals(itemList.get(0).getName()));
+        Assert.assertTrue(listFromJson.get(1).getName().equals(itemList.get(1).getName()));
     }
-
-
 }
