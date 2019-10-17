322392e3457078ca14245800a51ef4106e811938
Steget <sthugo@student.chalmers.se>
2019-10-17 16:12:20 +0200

    Make class final and hide constructor

--- src/main/java/storagesystem/model/IReservableFactory.java
+++ src/main/java/storagesystem/model/IReservableFactory.java
@@ -4,9 +4,12 @@ import javafx.scene.image.Image;
 
 /**
  * Factory for creating reservable things. Returns objects of the Interface
+ *
  * @see IReservable
  */
-public class IReservableFactory {
+public final class IReservableFactory {
+    private IReservableFactory() {
+    }
 
     public static IReservable createReservableItem(String name, String description, String userRequirements, int amount, Condition condition, boolean reservable, Location location, Image image) {
         return new Item(name, description, userRequirements, amount, condition, reservable, location, image);
